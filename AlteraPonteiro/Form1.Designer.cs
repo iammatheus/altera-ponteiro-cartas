b47956b5f9a02d813eb043a93aba74a85b1ec0c0
Matheus Ferreira Siqueira <matheusferreirasiqueira9@gmail.com>
2022-02-10 00:14:16 -0300

    Adicionar arquivos de projeto.

---
+++ AlteraPonteiro/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace AlteraPonteiro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listaDeCartas = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tOffset = new System.Windows.Forms.TextBox();
+            this.tPonteiro = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listaDeCartas
+            // 
+            this.listaDeCartas.FormattingEnabled = true;
+            this.listaDeCartas.ItemHeight = 15;
+            this.listaDeCartas.Location = new System.Drawing.Point(12, 43);
+            this.listaDeCartas.Name = "listaDeCartas";
+            this.listaDeCartas.Size = new System.Drawing.Size(350, 379);
+            this.listaDeCartas.TabIndex = 0;
+            this.listaDeCartas.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.salvarComoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "&Arquivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.abrirToolStripMenuItem.Text = "&Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.AbrirToolStripMenuItem_Click);
+            // 
+            // salvarComoToolStripMenuItem
+            // 
+            this.salvarComoToolStripMenuItem.Name = "salvarComoToolStripMenuItem";
+            this.salvarComoToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.salvarComoToolStripMenuItem.Text = "&Salvar como...";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.sairToolStripMenuItem.Text = "&Sair";
+            // 
+            // tOffset
+            // 
+            this.tOffset.Location = new System.Drawing.Point(442, 230);
+            this.tOffset.Name = "tOffset";
+            this.tOffset.Size = new System.Drawing.Size(100, 23);
+            this.tOffset.TabIndex = 2;
+            this.tOffset.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tPonteiro
+            // 
+            this.tPonteiro.Location = new System.Drawing.Point(585, 230);
+            this.tPonteiro.Name = "tPonteiro";
+            this.tPonteiro.Size = new System.Drawing.Size(100, 23);
+            this.tPonteiro.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(442, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Offset:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(585, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ponteiro:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(559, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tPonteiro);
+            this.Controls.Add(this.tOffset);
+            this.Controls.Add(this.listaDeCartas);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listaDeCartas;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.TextBox tOffset;
+        private System.Windows.Forms.TextBox tPonteiro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AlteraPonteiro/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlteraPonteiro
+{
+    public partial class Form1 : Form
+    {
+        public OpenFileDialog dialog;
+        public string caminhoArquivo;
+        public string caminhoTable;
+        public string nomeCompleto = "";
+        public string offsetCarta = "";
+        public string[] testeCaractere = new string[] { null };
+        string[,] traduzCaracteres = new string[2, 80] {
+                { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B", "0C", "0D", "0E", "0F", "10",
+                  "11", "12", "13", "14", "15", "16", "17", "18", "19", "1A", "1B", "1C", "1D", "1E", "1F", "20", "21",
+                  "22", "23", "24", "25", "26", "29", "2A", "2B", "2C", "2D", "2E", "2F", "30", "31", "32", "33", "34",
+                  "35", "36", "37", "38", "39", "3A", "3B", "3C", "3D", "3E", "3F", "40", "41", "42", "43", "44", "45",
+                  "46", "48", "4A", "4E", "4F", "50", "56", "57", "59", "5B", "FF", "55"},
+
+                {" ","e","t","a","o","i","n","s","r","h","l",".","d","u","m","c","g","y","w","f","p","b","k","!","A","v",
+                 "I","'","T","S","M",",","D","O","W","H","Y","E","R","G","L","C","N","B","?","P","-","F","z","K","j","U",
+                 "x","q","0","V","2","J","#","1","Q","Z","\u0022","3","5","&","/","7","X",":","4","6","$","*","+","8","9",
+                 "%","[ENTER]", "."}
+                };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ObterOffsetCarta(int index)
+        {
+            int indexCartaSelecionada = index;
+            int offSetAtual = 0x1C6801;
+
+            for (int i = 0; i < indexCartaSelecionada; i++)
+            {
+                offSetAtual += testeCaractere[i].Length + 1;
+            }
+
+            return offSetAtual.ToString("X");
+        }
+
+        public string ObterPonteiroCarta()
+        {
+            string quatroUlimosDigitosOffset = ObterOffsetCarta(listaDeCartas.SelectedIndex).Substring(2, 4);
+            int quatroUlimosDigitosOffsetInt = Convert.ToInt32(quatroUlimosDigitosOffset, 16);
+            int ponteiroCartaSelecionada = quatroUlimosDigitosOffsetInt - 0x800;
+            return ponteiroCartaSelecionada.ToString("X");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listaDeCartas.SelectedItem != null)
+            {
+                tPonteiro.Text = ObterPonteiroCarta();
+                tOffset.Text = ObterOffsetCarta(listaDeCartas.SelectedIndex);
+            }
+        }
+
+        private void AbrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dialog = new OpenFileDialog();
+            if (DialogResult.OK == dialog.ShowDialog())
+            {
+                caminhoArquivo = dialog.FileName;
+                caminhoTable = "C:/Users/Matheus Ferreira/Desktop/Isos/table.tbl";
+                CapturarNomeCarta();
+            }
+        }
+        private void CapturarNomeCarta()
+        {
+            FileStream caminho = new(caminhoArquivo, FileMode.OpenOrCreate);
+            int tamanhoByte = 10956;
+            byte[] espacosVazios = new byte[tamanhoByte];
+
+            caminho.Seek(0x1C6801, SeekOrigin.Begin);
+            while (caminho.Read(espacosVazios, 0, espacosVazios.Length) == tamanhoByte)
+            {
+                string espacoAtual = BitConverter.ToString(espacosVazios);
+                string[] teste2 = espacoAtual.Split("-");
+
+                string nomeCompletoHex = "";
+                string[] caralho = new string[] { };
+                for (int meuPau = 0; meuPau < teste2.Length; meuPau++)
+                {
+                    for (int i = 0; i < 80; i++)
+                    {
+                        if (traduzCaracteres[0, i] == teste2[meuPau])
+                        {
+                            nomeCompleto += traduzCaracteres[1, i];
+                            nomeCompletoHex += teste2[meuPau];
+                        }
+                    }
+                    if (teste2[meuPau] == "FF")
+                    {
+                        caralho = nomeCompleto.Split("[ENTER]");
+                        testeCaractere = caralho;
+                    }
+                }
+
+                foreach (string meuCu in caralho)
+                {
+                    listaDeCartas.Items.Add(meuCu);
+                }
+
+                listaDeCartas.Items.RemoveAt(722);
+                break;
+            }
+            caminho.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
